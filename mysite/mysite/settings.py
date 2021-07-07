058f453ddfb764a2c561e39b3a2489ca42d8efa7
louise-gautier <louise_gautier@orange.fr>
2021-07-07 20:02:59 +0200

    Paramétrage static_root

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -142,9 +142,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = "static"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
